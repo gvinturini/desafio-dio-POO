5e2e57fe0173bd851585feffa230a148c973956e
Giovanni Vinturini <go_vinturini@hotmail.com>
2021-12-14 21:38:31 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import com.dio.desafio.dominio.Conteudo;
-import com.dio.desafio.dominio.Curso;
-import com.dio.desafio.dominio.Mentoria;
+import com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -24,8 +22,46 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGiovanni = new Dev();
+        devGiovanni.setNome("Giovanni");
+        devGiovanni.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devGiovanni.getConteudosInscritos());
+        devGiovanni.progredir();
+        System.out.println("Conteúdos concluídos: " + devGiovanni.getConteudosConcluidos());
+        System.out.println("Conteúdos inscritos: " + devGiovanni.getConteudosInscritos());
+        System.out.println("XP: " + devGiovanni.calcularTotalXP());
+
+        System.out.println("--------------");
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("Conteúdos concluídos: " + devJoao.getConteudosConcluidos());
+        System.out.println("Conteúdos inscritos: " + devJoao.getConteudosInscritos());
+        System.out.println("XP: " + devJoao.calcularTotalXP());
+
     }
 }
+
+
+
+
+
+
+
+
+
